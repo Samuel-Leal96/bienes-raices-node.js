9d55f5c63fe6e0c975d88649383548d7e5f7d600
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-05-23 22:19:31 -0600

    Vistas y navegacion de la autenticación
    Se crean las vistas y logica de navegacion del auth

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,17 +1,24 @@
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
-        
+        pagina: 'Iniciar sesión'
     })
 }
 
 const formularioRegistro = (req, res) => {
     res.render('auth/registro', {
-        
+        pagina: 'Crear cuenta'
+    })
+}
+
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password', {
+        pagina: 'Recupera tu acceso'
     })
 }
 
 
 export{
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }
