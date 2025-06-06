d885840ba2e4ad45a9041bcc19782539cb8a8bb5
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-06 11:19:58 -0600

    Show map
    Se muestra el mapa con las coordenadas indicadas

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,12 @@
-alert('Compilacion concurrently')
+(function() {
+    const lat = 25.6859938;
+    const lng = - 100.3034103;
+    const mapa = L.map('mapa').setView([lat, lng ], 12);
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
