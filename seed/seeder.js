7044a55f6c3c91917814a5e681939c3a25868539
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-10 16:31:22 -0600

    Ejecutando seeder

--- seed/seeder.js
+++ seed/seeder.js
@@ -24,5 +24,11 @@ const importarDatos = async () => {
         console.log(error);
         exit(1); //* Terminamos todos los proceso corriendo en Node si es que hay un error
     }
+}
 
+//* Utilizamos argv para preguntar si en los argumentos del comando de la terminal viene -i entonces llamamos a importar datos
+//* Example: node ./seed/seeder.js -i, la primera posicion es node, la segunda es ./seed/seeder.js y -i es la tercera
+//* process quiere decir que es un proceso y lo ponemos porque estamos haciendo un script con node
+if(process.argv[2] === '-i'){
+    importarDatos();
 }
