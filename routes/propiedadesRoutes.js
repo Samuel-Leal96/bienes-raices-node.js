14a1658b18b0ec61b43c804341bae5d44393cc79
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-11 19:07:51 -0600

    Agregando resto de validaciones
    Se agregan las validaciones faltantes del formulario para crear una propiedad

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -5,8 +5,19 @@ const router = express.Router()
 
 router.get('/mis-propiedades', admin);
 router.get('/propiedades/crear', formCrearPropiedad);
+
 router.post('/propiedades/crear',
-    body('titulo').notEmpty().withMessage('El titulo del anuncio es obligatorio'), 
+    body('titulo').notEmpty().withMessage('El titulo del anuncio es obligatorio'),
+    body('descripcion')
+        .notEmpty().withMessage('La descripcion no puede ir vacía')
+        .isLength({max: 200}).withMessage('La descripción es muy larga'),
+    body('categoria').isNumeric().withMessage('Selecciona una categoria'),
+    body('precio').isNumeric().withMessage('Selecciona un rango de precios'),
+    body('habitaciones').isNumeric().withMessage('Selecciona la cantidad de habitaciones'),
+    body('estacionamiento').isNumeric().withMessage('Selecciona la cantidad de estacionamientos'),
+    body('banio').isNumeric().withMessage('Selecciona la cantidad de baños'),
+    body('banio').isNumeric().withMessage('Selecciona la cantidad de baños'),
+    body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa'),
     formGuardarPropiedad);
 
 
