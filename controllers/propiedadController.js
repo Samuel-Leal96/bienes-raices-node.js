b9d281adec63b11c516492178faba170bd28f7d1
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-11 18:57:05 -0600

    CSRF en el formulario y errores
    Se agrega CSRF y la validación de error en el controller

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,5 +1,7 @@
+import { validationResult } from 'express-validator'
 import Precio from '../models/Precio.js'
 import Categoria from '../models/Categoria.js'
+import { csrfAutenticate } from '../helpers/csrf-autenticate.js'
 
 const admin = (req, res) => {
     const errores = req.session.errores || [];
@@ -12,7 +14,9 @@ const admin = (req, res) => {
 }
 
 //* Formulario para crear una nueva propiedad
-const formularioCrearPropiedad = async (req, res)=>{
+const formCrearPropiedad = async (req, res) => {
+
+    console.log('Render crear propiedad');
 
     //* Consultar Modelo de Precio y Categoria
     const [categorias, precios] = await Promise.all([
@@ -24,7 +28,7 @@ const formularioCrearPropiedad = async (req, res)=>{
     const errores = req.session.errores || [];
     req.session.errores = null;
 
-    res.render('propiedades/crear',{
+    res.render('propiedades/crear', {
         pagina: 'Crear propiedad',
         errores,
         barra: true,
@@ -33,7 +37,45 @@ const formularioCrearPropiedad = async (req, res)=>{
     })
 }
 
-export{
+const formGuardarPropiedad = async (req, res) => {
+
+    //* Validación
+    let resultado = validationResult(req)
+
+    if (!resultado.isEmpty()) {
+
+        //* Consultar Modelo de Precio y Categoria
+        const [categorias, precios] = await Promise.all([
+            Categoria.findAll(),
+            Precio.findAll()
+        ])
+
+        req.session.errores = resultado.array();
+        req.session.categorias = categorias;
+        req.session.precios = precios;
+
+        return res.redirect('/propiedades/crear')
+
+        // return res.render('propiedades/crear', {
+        //     pagina: 'Crear propiedad',
+        //     barra: true,
+        //     categorias,
+        //     precios,
+        //     errores: resultado.array()
+        // })
+    }
+
+    //* Validar el token del form de CSRF
+    const autenticate = csrfAutenticate(req, res);
+
+    if (!autenticate) {
+        return;
+    }
+
+}
+
+export {
     admin,
-    formularioCrearPropiedad
+    formCrearPropiedad,
+    formGuardarPropiedad
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,9 +1,13 @@
 import express from "express"
-import { admin, formularioCrearPropiedad } from '../controllers/propiedadController.js'
+import { body } from 'express-validator'
+import { admin, formCrearPropiedad, formGuardarPropiedad } from '../controllers/propiedadController.js'
 const router = express.Router()
 
 router.get('/mis-propiedades', admin);
-router.get('/propiedades/crear', formularioCrearPropiedad);
+router.get('/propiedades/crear', formCrearPropiedad);
+router.post('/propiedades/crear',
+    body('titulo').notEmpty().withMessage('El titulo del anuncio es obligatorio'), 
+    formGuardarPropiedad);
 
 
 export default router
